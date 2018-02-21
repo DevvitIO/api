eaadb1ea9489b371e65f60d8f66e291ba981c7e5
Jonas Gierer <jgierer12@outlook.com>
2018-02-21 22:15:11 +0100

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
 require('mongoose');
 require('./db');
 
+app.use(cors());
+
 const router = new express.Router();
 
 require('./routes/leaderboard')(router);
-
 app.use('/api', router);
 
 app.listen(port);
